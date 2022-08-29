5aa2cc851c177705d7363a0fcfbf476493aae588
KxngMatthew <KxngMatthew@users.noreply.github.com>
2022-08-28 23:09:02 -0500

    Add project files/ first commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const form = document.querySelector('.quiz-form');
 //Select form base on class name result
 const result = document.querySelector('.result');
 
+
 form.addEventListener('submit',e => {
     e.preventDefault(); // prevent page from reloading after form is submitted
 
